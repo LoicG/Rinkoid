413527fffe328bff898cb2b70801b890f3b48645
Loic Geffroy <loic.geffroy@gmail.com>
2014-02-24 21:43:57 +0100

    Text font comestic

--- src/fr/sport/rinkoid/shedule/ScheduleAdapter.java
+++ src/fr/sport/rinkoid/shedule/ScheduleAdapter.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import fr.sport.rinkoid.R;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,15 +28,18 @@ public class ScheduleAdapter extends ArrayAdapter<Match> {
             .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.schedule_row, parent, false);
         Match match = items.get(position);
-        SetText(R.id.home, rowView, match.getHome());
-        SetText(R.id.score, rowView, match.getScore());
-        SetText(R.id.outside, rowView, match.getOutside());
+        int diff = Split(match.getScore());
+        SetText(R.id.home, rowView, match.getHome(), diff > 0);
+        SetText(R.id.score, rowView, match.getScore(), false);
+        SetText(R.id.outside, rowView, match.getOutside(), diff < 0);
         return rowView;
     }
 
-    private void SetText( int id, View rowView, String text ) {
+    private void SetText(int id, View rowView, String text, boolean bold) {
         TextView view = (TextView) rowView.findViewById(id);
         view.setText(text);
+        if(bold)
+            view.setTypeface(null, Typeface.BOLD);
     }
 
     public void Update(ArrayList<Match> items) {
@@ -46,5 +50,17 @@ public class ScheduleAdapter extends ArrayAdapter<Match> {
     @Override
     public int getCount() {
         return items.size();
+    }
+
+    private int Split(String score) {
+        score = score.replace(" ", "");
+        if(score.contains("-")) {
+            String[] splits = score.split("-");
+            if(splits.length == 2) {
+                return Integer.parseInt(splits[0]) -
+                       Integer.parseInt(splits[1]);
+            }
+        }
+        return 0;
     }
 }
