bcc10aa2f38b689c5659149653b468a07f78f1be
Loic Geffroy <loic.geffroy@masagroup.net>
2014-03-13 17:41:40 +0100

    Remove data test

--- src/fr/sport/rinkoid/MainActivity.java
+++ src/fr/sport/rinkoid/MainActivity.java
@@ -8,9 +8,6 @@ import download.InitialDownloadManager;
 
 import fr.sport.rinkoid.bar.SpinnerNavItem;
 import fr.sport.rinkoid.bar.TitleNavigationAdapter;
-import fr.sport.rinkoid.kickers.Kicker;
-import fr.sport.rinkoid.ranks.Rank;
-import fr.sport.rinkoid.shedule.Match;
 
 import android.app.ActionBar;
 import android.content.res.Resources;
@@ -69,8 +66,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.OnNaviga
         viewPager.setAdapter(pageAdapter);
         viewPager.setOnPageChangeListener(this);
         manager = new DownloadManager(db,pageAdapter);
-        // db.Clear();
-        // GenerateDataTest(db);
         Initialize(db);
     }
 
@@ -129,60 +124,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.OnNaviga
         break;
       }
       return true;
-    }
-
-    private void GenerateDataTest(DatabaseHelper db) {
-        ArrayList<Kicker> kickers = new ArrayList<Kicker>();
-        kickers.add(new Kicker("Bernard", 10, "eag"));
-        kickers.add(new Kicker("Albert", 10, "eag"));
-        kickers.add(new Kicker("Zorro",23 , "lorient"));
-        kickers.add(new Kicker("Loic", 5, "nantes"));
-        db.SaveKicker(kickers, Tools.N1);
-        kickers.clear();
-        kickers.add(new Kicker("Jean", 10, "psg"));
-        kickers.add(new Kicker("Pierre", 10, "brest"));
-        db.SaveKicker(kickers, Tools.N2N);
-        kickers.clear();
-        kickers.add(new Kicker("Jacques", 10, "quimper"));
-        db.SaveKicker(kickers, Tools.N2S);
-
-        ArrayList<Rank> ranks = new ArrayList<Rank>();
-        ranks.add(new Rank("equipe6", 9, 1, 5, 5, 3, 3));
-        ranks.add(new Rank("equipe1", 9, 12, 6, 5, 1, 12));
-        ranks.add(new Rank("equipe2", 9, 12, 5, 5, 3, 24));
-        ranks.add(new Rank("equipe3", 9, 12, 5, 5, 3, -25));
-        ranks.add(new Rank("equipe5", 9, 2, 5, 5, 3, 10));
-        ranks.add(new Rank("equipe4", 9, 2, 2, 1, 3, 10));
-        db.SaveRanks(ranks, Tools.N1);
-        ranks.clear();
-        ranks.add(new Rank("equipeN2N", 9, 2, 1, 3, 4, -6));
-        db.SaveRanks(ranks, Tools.N2N);
-        ranks.clear();
-        ranks.add(new Rank("equipeN2S", 9, 32, 1, 10, 5, 96));
-        db.SaveRanks(ranks, Tools.N2S);
-
-        ArrayList<Match> matchs = new ArrayList<Match>();
-        matchs.add(new Match("Equipe1", "2-1", "Equipe2", "2014-02-22"));
-        matchs.add(new Match("Equipe3", "5-5", "Equipe4", "2014-02-22"));
-        db.SaveMatchs(matchs, Tools.N1, 1);
-        matchs.clear();
-        matchs.add(new Match("Equipe1", "1-5", "Equipe3", "2014-02-22"));
-        matchs.add(new Match("Equipe2", "", "Equipe4", "2014-02-22"));
-        db.SaveMatchs(matchs, Tools.N1, 2);
-        matchs.clear();
-        matchs.add(new Match("Equipe1", "", "Equipe2", "2014-02-22"));
-        matchs.add(new Match("Equipe3", "", "Equipe4", "2014-02-22"));
-        db.SaveMatchs(matchs, Tools.N1, 3);
-        matchs.clear();
-        matchs.add(new Match("EquipeA", "1-9", "EquipeB", "2014-02-22"));
-        db.SaveMatchs(matchs, Tools.N2N, 1);
-        matchs.clear();
-        matchs.add(new Match("EquipeB", "5-9", "EquipeA", "2014-02-22"));
-        db.SaveMatchs(matchs, Tools.N2N, 2);
-        matchs.clear();
-        matchs.add(new Match("EquipeD", "", "EquipeC","2014-02-17"));
-        db.SaveMatchs(matchs, Tools.N2S, 1);
-        matchs.clear();
     }
 }
 
